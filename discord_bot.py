a8f4544cfa08a2a3da70776da087e61bcf465b5c
esihunc <77267639+esihunc@users.noreply.github.com>
2021-01-11 20:56:09 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -29,12 +29,12 @@ class chatbot(discord.Client):
             msg = message.content.split(" ")
             channel = message.channel
             
-            options = webdriver.ChromeOptions()
-            options.add_argument("--headless")
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--no-sandbox")
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--no-sandbox")
             
-            options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
             driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
             
             driver.get("http://bf.nexon.com/Rank/FastStart/List")
